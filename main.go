b2684d131d7c88e8e4e02649fddbb7908d26a61d
Jimmy <easelify@gmail.com>
2025-07-01 10:12:46 +0000

    feat: 使用html模板

--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 )
 
 func main() {
-    r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
-    r.Run() // 默认监听 :8080
+	r := gin.Default()
+	r.LoadHTMLGlob("web/template/*.html")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // 默认监听 :8080
 }
