81d237f611499c1e1c1fd814e28116b583e27949
Jimmy <easelify@gmail.com>
2025-07-02 00:36:14 +0000

    feat: 表单提交和返回

--- main.go
+++ main.go
@@ -19,5 +19,32 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	r.GET("/login", func(c *gin.Context) {
+		c.HTML(200, "login.html", gin.H{
+			"message": "所有字段为必填项",
+		})
+	})
+
+	r.POST("/login", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+		if username == "" || password == "" {
+			c.HTML(400, "login.html", gin.H{
+				"message": "用户名和密码不能为空",
+			})
+			return
+		}
+		if username == "admin" && password == "123456" {
+			c.HTML(200, "login.html", gin.H{
+				"message": "模拟登录成功",
+			})
+		} else {
+			c.HTML(401, "login.html", gin.H{
+				"message": "用户名或密码错误",
+			})
+		}
+	})
+
 	r.Run() // 默认监听 :8080
 }
